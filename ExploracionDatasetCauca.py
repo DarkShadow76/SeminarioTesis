82b140fb5832aee8a70f25fa09200f2f10a9b675
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-05-17 12:09:34 -0500

    undersampling

--- ExploracionDatasetCauca.py
+++ ExploracionDatasetCauca.py
@@ -5,11 +5,14 @@ Created on Tue Apr 16 09:44:21 2024
 
 @author: tori
 """
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+<<<<<<< HEAD
 from sklearn.svm import SVR
+=======
+from sklearn.utils import resample
+>>>>>>> 0c9befc (undersampling)
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 # from sklearn.metrics import accuracy_score, classification_report
@@ -18,8 +21,25 @@ from sklearn.metrics import mean_squared_error, r2_score
 excel_file = "./Dataset_Year_2020.xlsx"
 data = pd.read_excel(excel_file)
 
-print("Información general de los datos(Antes):")
+print("Información general de los datos (Antes):")
 print(data.info())
+
+## Tecnica OverSampling
+# Sparar Clases mayoritarias y minoritarias
+majority_class = data[data['Burned transformers 2020'] == 0]
+minority_class = data[data['Burned transformers 2020'] == 1]
+
+# Oversample en la clase minoritaria
+majority_class_undersampled = resample(majority_class,
+                                       replace=False,  # sample without replacement
+                                       n_samples=len(minority_class),  # match minority class size
+                                       random_state=42)  # for reproducibility
+
+# Cambinear ambas clases
+data = pd.concat([majority_class_undersampled, minority_class])
+
+# Nuevo conteo
+print(data['Burned transformers 2020'].value_counts())
 
 # Obtén las categorías únicas de las columnas categóricas
 categorias_cliente = data['Type of clients'].unique()
@@ -35,11 +55,14 @@ data['Type of installation'] = data['Type of installation'].astype('category').c
 scaler = StandardScaler()
 
 # Seleccionamos solo las columnas numéricas para estandarizar
-columnas_numericas = data.select_dtypes(include=['int64','int8', 'float64']).columns
+columnas_std = data.columns
 
 # Estandarizamos las columnas seleccionadas
-data[columnas_numericas] = scaler.fit_transform(data[columnas_numericas])
+normalized_data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
 
+# data.columns = scaler.fit_transform(data[columnas_std])
+
+data = normalized_data
 
 print("Información general de los datos(Despues):")
 print(data.info())
@@ -48,6 +71,7 @@ print("\nEstadísticas descriptivas de las variables numéricas:")
 print(data.describe())
 
 
+# Matriz
 corr_matrix = data.corr()
 plt.figure(figsize=(12, 8))
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", annot_kws={"size": 10})
@@ -55,14 +79,16 @@ plt.title('Matriz de correlación')
 plt.show()
 
 # Seleccionar todas las columnas numéricas excepto 'Burned transformers 2020'
-columnas_numericas = data.select_dtypes(include=['int64', 'int8', 'float64']).columns.drop('Burned transformers 2020')
+columnas_numericas = data.select_dtypes(include=['int64', 'float64']).columns.drop('Burned transformers 2020')
 
 # Crear el box plot
-plt.figure(figsize=(12, 8))
+
+plt.figure(figsize=(20, 16))
 sns.boxplot(data=data[columnas_numericas])
-plt.title('Box plot de las columnas numéricas')
+plt.title('Box plot de las variables')
 plt.xlabel('Columnas')
 plt.ylabel('Valores')
+<<<<<<< HEAD
 plt.xticks(rotation=45)  # Rotar las etiquetas del eje x para una mejor visualización
 plt.show()
 
@@ -85,3 +111,9 @@ mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print("Error cuadrático medio (MSE):", mse)
 print("Coeficiente de determinación (R^2):", r2)
+=======
+plt.xticks(rotation=45)
+plt.show()
+
+print(data.head())
+>>>>>>> 0c9befc (undersampling)
