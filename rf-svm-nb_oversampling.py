1077a0c1d82e063a20ded787cfee0f198ab0bef6
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-06-03 23:56:30 -0500

    minor changes

--- rf-svm-nb_oversampling.py
+++ rf-svm-nb_oversampling.py
@@ -2,8 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from imblearn.over_sampling import SMOTE
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import ADASYN
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, accuracy_score, recall_score, confusion_matrix, classification_report, roc_auc_score
 
 # Leer el archivo Excel
 excel_file = "./Dataset_Year_2020.xlsx"
@@ -16,10 +23,30 @@ data = pd.get_dummies(data, columns=['Type of clients', 'Type of installation'])
 X = data.drop(columns=['Burned transformers 2020'])
 y = data['Burned transformers 2020'].astype(int)
 
+# Definir la cantidad de muestras adicionales que quieres generar
+desired_samples = 9000
+
+# Calcular la proporción correspondiente
+total_minority_class = y.value_counts()[1] + desired_samples
+sampling_strategy = total_minority_class / y.value_counts()[0]
+
+# Aplicar SMOTE para oversampling
+smote = SMOTE(sampling_strategy=sampling_strategy, random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X, y)
+
+"""
+# Aplicar ADASYN para oversampling
+adasyn = ADASYN(sampling_strategy='minority', random_state=42)
+X_resampled, y_resampled = adasyn.fit_resample(X, y)
+
+# Aplicar SMOTE para oversampling
+smote = SMOTE(sampling_strategy='minority', random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X, y)
+
 # Aplicar RandomOverSampler para oversampling
 ros = RandomOverSampler(sampling_strategy='minority', random_state=42)
 X_resampled, y_resampled = ros.fit_resample(X, y)
-
+"""
 # Actualizar el DataFrame con los datos oversampleados
 data = pd.concat([pd.DataFrame(X_resampled, columns=X.columns), 
                   pd.DataFrame(y_resampled, columns=['Burned transformers 2020'])], axis=1)
@@ -44,17 +71,73 @@ float_columns = data.select_dtypes(include=['float64']).columns
 scaler = StandardScaler()
 data[float_columns] = scaler.fit_transform(data[float_columns])
 
-print(data.info())
+# Tratar valores outliers
 
-# Imprimir el máximo, mínimo, media y desviación estándar de cada columna float64
-for col in float_columns:
-    max_value = data[col].max()
-    min_value = data[col].min()
-    mean_value = data[col].mean()
-    std_value = data[col].std()
-    print(f"Columna: {col}")
-    print(f"  Máximo: {max_value}")
-    print(f"  Media: {mean_value}")
-    print(f"  Mínimo: {min_value}")
-    print(f"  Desviación Estándar: {std_value}")
-    print()
+# Separar los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['Burned transformers 2020']), data['Burned transformers 2020'], test_size=0.3, random_state=42)
+
+# Entrenar y evaluar SVM
+svm = SVC(probability=True)
+svm.fit(X_train, y_train)
+y_pred_svm = svm.predict(X_test)
+y_prob_svm = svm.predict_proba(X_test)[:, 1]
+
+f_score_svm = f1_score(y_test, y_pred_svm)
+accuracy_svm = accuracy_score(y_test, y_pred_svm)
+recall_svm = recall_score(y_test, y_pred_svm)
+conf_matrix_svm = confusion_matrix(y_test, y_pred_svm)
+roc_auc_svm = roc_auc_score(y_test, y_prob_svm)
+
+print("\nMetricas para el modelo SVM")
+print("F-score:", f_score_svm)
+print("Exactitud:", accuracy_svm)
+print("Recall:", recall_svm)
+print("Matriz de Confusión:")
+print(conf_matrix_svm)
+print("Reporte de Clasificación:")
+print(classification_report(y_test, y_pred_svm))
+print("AUC:", roc_auc_svm)
+
+# Entrenar y evaluar Naive Bayes
+nb = GaussianNB()
+nb.fit(X_train, y_train)
+y_pred_nb = nb.predict(X_test)
+y_prob_nb = nb.predict_proba(X_test)[:, 1]
+
+f_score_nb = f1_score(y_test, y_pred_nb)
+accuracy_nb = accuracy_score(y_test, y_pred_nb)
+recall_nb = recall_score(y_test, y_pred_nb)
+conf_matrix_nb = confusion_matrix(y_test, y_pred_nb)
+roc_auc_nb = roc_auc_score(y_test, y_prob_nb)
+
+print("\nMetricas para el modelo Naive Bayes")
+print("F-score:", f_score_nb)
+print("Exactitud:", accuracy_nb)
+print("Recall:", recall_nb)
+print("Matriz de Confusión:")
+print(conf_matrix_nb)
+print("Reporte de Clasificación:")
+print(classification_report(y_test, y_pred_nb))
+print("AUC:", roc_auc_nb)
+
+# Entrenar y evaluar Random Forest
+rf = RandomForestClassifier(random_state=42)
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+y_prob_rf = rf.predict_proba(X_test)[:, 1]
+
+f_score_rf = f1_score(y_test, y_pred_rf)
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+recall_rf = recall_score(y_test, y_pred_rf)
+conf_matrix_rf = confusion_matrix(y_test, y_pred_rf)
+roc_auc_rf = roc_auc_score(y_test, y_prob_rf)
+
+print("\nMetricas para el modelo Random Forest")
+print("F-score:", f_score_rf)
+print("Exactitud:", accuracy_rf)
+print("Recall:", recall_rf)
+print("Matriz de Confusión:")
+print(conf_matrix_rf)
+print("Reporte de Clasificación:")
+print(classification_report(y_test, y_pred_rf))
+print("AUC:", roc_auc_rf)
